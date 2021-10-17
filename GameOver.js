7bfbcf5e73a3613501efe34b7e03008db4ed83a6
Shohanur Rahaman <shohan4556@gmail.com>
2021-10-17 13:25:31 +0600

    Update GameOver.js

--- GameOver.js
+++ GameOver.js
@@ -10,13 +10,18 @@ export default class GameOver extends Phaser.Scene {
     const height = this.scale.height;
 
     this.add
-      .text(width * 0.5, height * 0.5, "Game Over ! \nPress Space to Restart", {
+      .text(width * 0.5, height * 0.5, "Game Over ! Click to Restart", {
         fontSize: 26,
+        color: "#000",
       })
       .setOrigin(0.5);
 
-    this.input.keyboard.once("keydown-SPACE", () => {
+    this.input.once("pointerdown", () => {
       this.scene.start("game");
     });
+
+    // this.input.keyboard.once("keydown-SPACE", () => {
+    //   this.scene.start("game");
+    // });
   }
 }
